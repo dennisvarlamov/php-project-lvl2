984d4f2cbc26f36a0898ea286da626d9c8604dce
Dennis <dennisvarlamov@gmail.com>
2020-10-11 19:00:35 +0300

    added parser

---
+++ src/Parser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Differ\Parser;
+
+use Symfony\Component\Yaml\Yaml;
+
+function    parse(string $fileData, string $fileFormat) : array
+{
+    switch ($fileFormat) {
+        case 'json' :
+            return json_decode($fileData, true);
+        case 'yaml':
+        case 'yml' :
+            return Yaml::parse($fileData);
+        default:
+            throw new \Esception("Format {$fileFormat} is not supported");
+    }
+}
